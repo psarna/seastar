1e5b2634f0693cc2e6d3f237e9b1425164d9f41f
Krzysztof Małysa <varqox@gmail.com>
2019-12-28 20:55:45 +0100

    fs: add headers with unit types and operations on them
    - range.hh: numeric range abstraction
    - bitwise.hh: bitwise operations
    - unit_types.hh: basic unit types
    - cluster_utils.hh: operations on cluster ids (converting cluster ids to
        offsets and back)
    - inode_utils.hh: operations on inode type (extracting shard_no from
        inode and allocating a new inode)
    
    Signed-off-by: Krzysztof Małysa <varqox@gmail.com>

---
+++ src/fs/cluster_utils.hh
@@ -0,0 +1,39 @@
+/*
+ * This file is open source software, licensed to you under the terms
+ * of the Apache License, Version 2.0 (the "License").  See the NOTICE file
+ * distributed with this work for additional information regarding copyright
+ * ownership.  You may not use this file except in compliance with the License.
+ *
+ * You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+/*
+ * Copyright (C) 2019 ScyllaDB
+ */
+
+#pragma once
+
+#include "seastar/fs/bitwise.hh"
+#include "seastar/fs/unit_types.hh"
+
+namespace seastar::fs {
+
+inline cluster_id_t offset_to_cluster_id(disk_offset_t offset, disk_offset_t cluster_size) noexcept {
+    assert(is_power_of_2(cluster_size));
+    return div_by_power_of_2(offset, cluster_size);
+}
+
+inline disk_offset_t cluster_id_to_offset(cluster_id_t cluster_id, disk_offset_t cluster_size) noexcept {
+    assert(is_power_of_2(cluster_size));
+    return mul_by_power_of_2(cluster_id, cluster_size);
+}
+
+} // namespace seastar::fs
