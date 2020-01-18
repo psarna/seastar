090626d9dbf59979bd00403ec612ad2bae7e47e2
Aleksander Sorokin <ankezy@gmail.com>
2020-01-19 00:57:37 +0100

    fs: add a method returning the size in block_device abstraction
    Signed-off-by: Aleksander Sorokin <ankezy@gmail.com>

--- tests/unit/fs_block_device_mocker.hh
+++ tests/unit/fs_block_device_mocker.hh
@@ -21,6 +21,8 @@
 
 #pragma once
 
+#include "fs/units.hh"
+
 #include <seastar/fs/block_device.hh>
 #include <string>
 
@@ -51,6 +53,10 @@ public:
 
     future<> flush() noexcept override {
         return make_ready_future<>();
+    }
+
+    future<disk_offset_t> size() noexcept override {
+        return make_ready_future<disk_offset_t>(buf.size());
     }
 
     future<> close() noexcept override {
